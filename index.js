bbc7f180a07cd241b202370e448c4044237ab8c8
nadiaell <nadiaell@hotmail.com>
2024-12-02 00:28:30 -0300

    agregue sin  texto superior e inferior en java

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function $(selector){
+function $(selector) {
       return document.querySelector(selector);
 }
 
@@ -6,44 +6,59 @@ function $(selector){
 const $url = document.querySelector("#url");
 const $imgMeme = document.querySelector("#imgMeme");
 
-$url.addEventListener("input",()=>{
+$url.addEventListener("input", () => {
       $imgMeme.innerHTML = `<img src="${$url.value}" >`
 })
 
 const $fondo = document.querySelector("#fondo");
 
-$fondo.addEventListener("input",()=>{
-      $imgMeme.style.backgroundColor =`#${$fondo.value}`
+$fondo.addEventListener("input", () => {
+      $imgMeme.style.backgroundColor = `#${$fondo.value}`
 
 })
 
 const $brillo = document.querySelector("#brillo");
 
 
-$brillo.addEventListener =("input",()=>{
-      $url.style.filter ="brightness(${$brillo.value})";
+$brillo.addEventListener = ("input", () => {
+      $url.style.filter = "brightness(${$brillo.value})";
 });
 
-const $contraste= $("#contraste");
-$contraste.addEventListener("input",()=>{
-      $url.style.filter=`constrast(${$conntraste.value})`
+const $contraste = $("#contraste");
+$contraste.addEventListener("input", () => {
+      $url.style.filter = `constrast(${$conntraste.value})`
 })
- //------aside -texto superior---------//
- const $topText =$("#top-tex");
- const $topTextMain =$("#topTextMain");
+//------aside -texto superior---------//
+const $topText = $("#top-tex");
+const $topTextMain = $("#topTextMain");
+const $sinTextoSuperior = $("#sinTextoSuperior")
 
- $topText.addEventListener("input",(e)=>{
-      $topTextMain.innerText = e.target.value
+$topText.addEventListener("input", (e) => {
+      $topTextMain.innerText = e.target.value;
 
- })
- //---------aside.texto.infeior------------//
- const $textoInferior = $("#textoInferior");
- const $textoInferiorMain =$("#topText");
+})
+$sinTextoSuperior.addEventListener("change", () => {
+      if ($sinTextoSuperior.checked) {
+            $topTextMain.style.color = "transparent"
+      } else {
+            $topTextMain.style.color = "black"
+      };
+})
+//---------aside.texto.infeior------------//
+const $textoInferior = $("#textoInferior");
+const $textoInferiorMain = $("#topText");
+const $sinTextoInferior = $("#sinTextoInferior");
 
- $textoInferior.addEventListener("input",(e)=>{
+$textoInferior.addEventListener("input", (e) => {
       $textoInferiorMain.innerText = e.target.value
- })
+})
+$sinTextoInferior.addEventListener("change", () => {
+      if ($sinTextoInferior.checked) {
+            $textoInferiorMain.style.color = "transparent"
+      } else {
+            $textoInferiorMain.style.color = "black"
+      };
+})
 
- 
 
 
